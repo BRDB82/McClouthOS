d372c5e9b45189475dab9286968141fe774ce850
BRDB82 <b.r.debrouwer@gmail.com>
2025-10-04 10:48:26 +0200

    Create test-rep-install.sh

---
+++ RHEL/test-rep-install.sh
@@ -0,0 +1,43 @@
+ #!/bin/bash
+ VERSION=$(curl -s https://developers.redhat.com/products/rhel/download | \
+            grep -oE 'Red Hat Enterprise Linux [0-9]+\.[0-9]+' | \
+            grep -oE '[0-9]+\.[0-9]+' | \
+            sort -V | tail -1 | sed 's:/$::')
+  
+[-d /etc/yum.repos.d ] || mkdir /etc/yum.repos.d
+[ -d /tmp/rhel.repos.d ] || mkdir /tmp/rhel.repos.d
+
+if [ ! -f /tmp/rhel.repos.d/BaseOS.repo ]; then
+	{
+	echo "[rhel-baseos]"
+	echo "name=Red Hat Enterprise Linux $VERSION - BaseOS"
+	echo "baseurl=https://cdn.redhat.com/content/dist/rhel/$VERSION/x86_64/BaseOS/production/os/"
+	echo "enabled=1"
+	echo "gpgcheck=0"
+	} > /tmp/rhel.repos.d/BaseOS.repo
+fi
+
+echo "releasever=$VERSION" >> /etc/dnf/dnf.conf
+echo "$VERSION" > /etc/dnf/vars/releasever
+echo "x86_64" > /etc/dnf/vars/basearch
+echo "production" > /etc/dnf/vars/rltype
+
+rm -f /etc/yum.repos.d/*.repo
+
+for f in /tmp/rhel.repos.d/*.repo; do
+	ln -s "$f" /etc/yum.repos.d/$(basename "$f")
+done
+
+read -p "Red Hat account: " RHELuser
+read -s -p "Red Hat password: " RHELpasswd
+echo
+subscription-manager register --username="RHELuser" --password="RHELpasswd"
+sleep 5
+unset RHELuser
+unset RHELpasswd
+subscription-manager attach --auto
+sleep 5
+dnf --setopt=reposdir=/tmp/rhel.repos.d update -y
+dnf --setopt=reposdir=/tmp/rhel.repos.d clean all
+dnf --setopt=reposdir=/tmp/rhel.repos.d makecache
+dnf --setopt=reposdir=/tmp/rhel.repos.d install -y rpm
