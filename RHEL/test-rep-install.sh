8565e267a58303d2786406cf5b69267751f6cf02
BRDB82 <b.r.debrouwer@gmail.com>
2025-10-04 18:52:17 +0200

    Update test-rep-install.sh

--- RHEL/test-rep-install.sh
+++ RHEL/test-rep-install.sh
@@ -139,4 +139,4 @@ dnf --setopt=reposdir=/tmp/rhel.repos.d install -y ca-certificates || true
 dnf --setopt=reposdir=/tmp/rhel.repos.d install -y rpm
 
 echo "=== RHEL registration and repo setup complete. You can now install packages. ==="
-#update1833
+#update1852
