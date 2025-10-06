f6398b554968be855043eaf6132b0323c7064a1b
BRDB82 <b.r.debrouwer@gmail.com>
2025-10-06 22:00:28 +0200

    Update mccos-base-install.sh

--- RHEL/mccos-base-install.sh
+++ RHEL/mccos-base-install.sh
@@ -2,17 +2,18 @@
 
 #init
 mccos_ipdevices=0
+declare -a mccos_ip_port
+declare -a mccos_ip_address
 
 #loop through all interfaces iwth IPv4
 while NIC= read -r line; do
   iface=$(echo "$line" | awk '{print $2}')
   ipaddr=$(echo "$line" | awk '{print $4}')
 
-  eval "mccos_ip($mccos_ipdevices)_port='$iface'"
-  eval "mccos_ip($mccos_ipdevices)_address='$ipaddr'"
-
-  ((mccos_ipdevices++))
+  mccos_ip_port+=("'$iface'")
+  mccos_ip_address+=("'$ipaddr'")
 done < <(ip -o -4 addr show)
+mccos_ipdevices=${#mccos_ip_port[@]}
 
 echo "Total interfaces with IPv4: $mccos_ipdevices"
 for ((i=0; i<mccos_ipdevices; i++)); do
