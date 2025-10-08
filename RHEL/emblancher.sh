31c1206182d65df4cc19a886f93782ff1ff98575
BRDB82 <b.r.debrouwer@gmail.com>
2025-10-08 13:39:27 +0200

    Update emblancher.sh

--- RHEL/emblancher.sh
+++ RHEL/emblancher.sh
@@ -24,6 +24,60 @@
 logfile="/root/emblancher.log"
 pidfile="/var/run/emblancher.pid"
 
+select_option() {
+    local options=("$@")
+    local num_options=${#options[@]}
+    local selected=0
+    local last_selected=-1
+
+    while true; do
+        # Move cursor up to the start of the menu
+        if [ $last_selected -ne -1 ]; then
+            echo -ne "\033[${num_options}A"
+        fi
+
+        if [ $last_selected -eq -1 ]; then
+            echo "Please select an option using the arrow keys and Enter:"
+        fi
+        for i in "${!options[@]}"; do
+            if [ "$i" -eq $selected ]; then
+                echo "> ${options[$i]}"
+            else
+                echo "  ${options[$i]}"
+            fi
+        done
+
+        last_selected=$selected
+
+        # Read user input
+        read -rsn1 key
+        case $key in
+            $'\x1b') # ESC sequence
+                read -rsn2 -t 0.1 key
+                case $key in
+                    '[A') # Up arrow
+                        ((selected--))
+                        if [ $selected -lt 0 ]; then
+                            selected=$((num_options - 1))
+                        fi
+                        ;;
+                    '[B') # Down arrow
+                        ((selected++))
+                        if [ $selected -ge $num_options ]; then
+                            selected=0
+                        fi
+                        ;;
+                esac
+                ;;
+            '') # Enter key
+                break
+                ;;
+        esac
+    done
+
+    return $selected
+}
+
 if [ "$1" == "--update" ]; then
 	curl -fsSL "https://raw.githubusercontent.com/BRDB82/McClouthOS/main/RHEL/emblancher.sh" -o "/usr/bin/emblancher.new" || {
 		echo "update failed"
@@ -63,5 +117,20 @@ fi
 if [ ! -d "/sys/firmware/efi" ]; then
 	echo "Legacy boot is not supported"
 fi
+
+echo -ne "
+Please select keyboard layout from this list
+"
+    # These are default key maps commonly supported on RHEL
+    options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru se sg ua uk)
+
+    select_option "${options[@]}"
+    keymap=${options[$?]}
+
+    echo -ne "Your keyboard layout: ${keymap} \n"
+    export KEYMAP=$keymap
+
+    # Apply the selected keymap using localectl
+    localectl set-keymap "$keymap"
 
 #001-0002
