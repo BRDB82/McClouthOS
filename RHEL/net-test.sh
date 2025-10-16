346faf26d1e37db87257c50763bd1931a63e0a2e
BRDB82 <b.r.debrouwer@gmail.com>
2025-10-16 08:58:23 +0200

    Update net-test.sh

--- RHEL/net-test.sh
+++ RHEL/net-test.sh
@@ -200,11 +200,7 @@ fi
 	else
 		#gonna assume we'll have an active NIC, there is in my case, because else, how could we've gotten this far anyway, right? ;-)
   			echo "[DEBUG-L001]::$INTERFACE_NAME"
-		nmcli connection modify "$INTERFACE_NAME" ipv4.method manual
-		nmcli connection modify "$INTERFACE_NAME" ipv4.method manual
-		nmcli connection modify "$INTERFACE_NAME" ipv4.addresses "$IP_ADDRESS/$SUBNET_MASK"
-		nmcli connection modify "$INTERFACE_NAME" ipv4.gateway "$GATEWAY"
-		nmcli connection modify "$INTERFACE_NAME" ipv4.dns "$DNS_SERVERS"
+		nmcli connection modify "$INTERFACE_NAME" ipv4.method manual ipv4.addresses "$IP_ADDRESS/$SUBNET_MASK" ipv4.gateway "$GATEWAY" ipv4.dns "$DNS_SERVERS"
 		nmcli connection up "$INTERFACE_NAME"
 	fi
 
