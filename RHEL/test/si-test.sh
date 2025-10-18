5c6d4e4ed2547ece54a87ab94ef7904b1dcd786e
BRDB82 <b.r.debrouwer@gmail.com>
2025-10-19 01:37:37 +0200

    Update si-test.sh

--- RHEL/test/si-test.sh
+++ RHEL/test/si-test.sh
@@ -88,15 +88,15 @@ fi
 echo "Starting installer, one moment..."
 
 if [ "$1" == "--update" ]; then
-        curl -fsSL "https://raw.githubusercontent.com/BRDB82/McClouthOS/main/RHEL/net-test.sh" -o "net-test.new" || {
+        curl -fsSL "https://raw.githubusercontent.com/BRDB82/McClouthOS/main/RHEL/si-test.sh" -o "si-test.new" || {
                 echo "update failed"
-            rm "net-test.new"
+            rm "si-test.new"
             exit 1
         }
-	chmod +x "net-test.new"
-        mv -f "net-test.new" "net-test.sh"
+	chmod +x "si-test.new"
+        mv -f "si-test.new" "si-test.sh"
         echo ""
-        echo "Update installed. Please restart net-test.sh."
+        echo "Update installed. Please restart si-test.sh."
         exit 0
 fi
 
@@ -143,7 +143,41 @@ INSTALL_TYPE="server"
 if [ "$(systemd-detect-virt)" = "kvm" ]; then
         echo "We are running on virtual hardware"
 elif [ "$(systemd-detect-virt)" = "none" ]; then
-        echo "We are running on physical hardware" 
+        MIN_HDDS=4
+		MIN_CACHE_SSD=1
+
+		OOT_PARTITION=$(df -P / | awk 'END{print $1}')
+        ROOT_DISK_NAME=$(lsblk -no pkname "$ROOT_PARTITION" | head -n 1)
+
+       	if [ -z "$ROOT_DISK_NAME" ]; then
+                ROOT_DISK_NAME=$(basename "$ROOT_PARTITION")
+        fi
+
+		if [ -z "$ROOT_DISK_NAME" ]; then
+                echo "Could not determine root disk."
+                exit
+        fi
+
+		while read -r device rota; do
+                #skip the root disk
+                if [ "$device" == "$ROOT_DISK_NAME" ]; then
+                        continue
+                fi
+                if [ "$rota" -eq 1 ]; then
+                        HDD_DEVICES+=("/dev/$device")
+                elif [ "$rota"  -eq 0 ]; then
+                        CACHE_DEVICES+=("/dev/$device")
+                fi
+        done < <(lsblk -d -o NAME,ROTA --noheadings)
+
+        if [ "${#HDD_DEVICES[@]}" -ge "$MIN_HDDS" ] && [ "${#CACHE_DEVICES[@]}" -ge "$MIN_CACHE_SSD" ]; then
+                export HDD_DEVICES_EXPORTED="$(declare -p HDD_DEVICES)"
+                export CACHE_DEVICES_EXPORTED="$(declare -p CACHE_DEVICES)"
+        else
+            	echo "Insufficient drives found. Require at least $MIN_HDDS HDDs and $MIN_SSDS SSDs."
+                exit 1
+        fi
+
 fi
 
 
@@ -157,3 +191,16 @@ fi
                     Enabling Server Services
 -------------------------------------------------------------------------
 "
+echo "* FILE STORAGE SERVER"
+if [[ -n "$HDD_DEVICES_EXPORTED" ]]; then
+        eval "$HDD_DEVICES_EXPORTED"
+        eval "$CACHE_DEVICES_EXPORTED"
+
+        echo "HDD Devices:"
+        printf " - %s\n" "${HDD_DEVICES[@]}"
+        echo "Cache Devices:"
+        printf " - %s\n" "${CACHE_DEVICES[@]}"
+else
+        echo "Arays were not passed or could not be recreated."
+        exit 1
+fi
