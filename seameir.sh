a7b297119e87c387fd23e8b28588fdd37e84fa24
BRDB82 <b.r.debrouwer@gmail.com>
2026-02-15 19:34:52 +0100

    Update seameir.sh

--- seameir.sh
+++ seameir.sh
@@ -4,6 +4,60 @@
 
 # "Luck is what happens when preparation meets opportunity."
 
+select_option() {
+    local options=("$@")
+    local num_options=${#options[@]}
+    local selected=0
+    local last_selected=-1
+
+    while true; do
+        # Move cursor up to the start of the menu
+        if [ $last_selected -ne -1 ]; then
+            echo -ne "\033[${num_options}A"
+        fi
+
+        if [ $last_selected -eq -1 ]; then
+            echo "Please select an option using the arrow keys and Enter:"
+        fi
+        for i in "${!options[@]}"; do
+            if [ "$i" -eq $selected ]; then
+                echo "> ${options[$i]}"
+            else
+                echo "  ${options[$i]}"
+            fi
+        done
+
+        last_selected=$selected
+
+        # Read user input
+        read -rsn1 key
+        case $key in
+            $'\x1b') # ESC sequence
+                read -rsn2 -t 0.1 key
+                case $key in
+                    '[A') # Up arrow
+                        ((selected--))
+                        if [ $selected -lt 0 ]; then
+                            selected=$((num_options - 1))
+                        fi
+                        ;;
+                    '[B') # Down arrow
+                        ((selected++))
+                        if [ $selected -ge $num_options ]; then
+                            selected=0
+                        fi
+                        ;;
+                esac
+                ;;
+            '') # Enter key
+                break
+                ;;
+        esac
+    done
+
+    return $selected
+}
+
 #root-check
 if [[ "$(id -u)" != "0" ]]; then
     echo -ne "!!ERROR!! This script must be as root, not with sudo.\n"
@@ -104,4 +158,48 @@ while true
     done
 export uPASSWORD=$PASSWORD1
 
+while true
+	do
+    	read -r -p "Please enter new hostname (e.g. 11-103-W001): " name_of_machine
+		# Regex breakdown:
+    	# ^[1-3][1-2]    -> Country (1-3) and City (1-2)
+   		# -              -> Hyphen
+   		# [0-1]{2}[0-3]  -> Env(0-1), HW(0-1), Role(0-3)
+    	# -              -> Hyphen
+    	# [NSW]          -> Device type (Network, Server, Workstation)
+    	# [0-9]{3}$      -> Exactly 3 digits
+    
+    	if [[ "$name_of_machine" =~ ^[1-3][1-2]-[0-1]{2}[0-3]-[NSW][0-9]{3}$ ]]
+    	then
+        	break
+    	fi
+
+    	echo "Format error! Ensure it matches: [Location]-[Spec]-[DeviceID]"
+    	read -r -p "Do you still want to save it? (y/n): " force
+    	[[ "${force,,}" == "y" ]] && break
+    done
+export NAME_OF_MACHINE=$name_of_machine
+
+echo -ne "
+------------------------------------------------------------------------
+    THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK
+    Please make sure you know what you are doing because
+    after formatting your disk there is no way to get data back
+    *****BACKUP YOUR DATA BEFORE CONTINUING*****
+    ***I AM NOT RESPONSIBLE FOR ANY DATA LOSS***
+------------------------------------------------------------------------
+
+"
+
+    PS3='
+    Select the disk to install on: '
+    options=($(lsblk -n --output TYPE,KNAME,SIZE | awk '$1=="disk"{print "/dev/"$2"|"$3}'))
+
+    select_option "${options[@]}"
+    disk=${options[$?]%|*}
+
+    echo -e "\n${disk%|*} selected \n"
+    export DISK=${disk%|*}
+
+    drivessd
 
